78fb05eb7b2466d2ea2b28c2d48074eec44152a7
preeyamon <65010914629@msu.ac.th>
2025-02-25 12:46:00 +0700

    j

--- connectdb.php
+++ connectdb.php
@@ -1,8 +1,8 @@
 <?php
 $host ="localhost";
 $user = "root";
-$pwd = "";
-$db = "shop";
+$pwd = "qq123456";
+$db = "shop4629";
 
 $conn = mysqli_connect($host,$user,$pwd) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 mysqli_select_db($conn,$db)or die ("เลือกฐานข้อมูลไม่ได้");
